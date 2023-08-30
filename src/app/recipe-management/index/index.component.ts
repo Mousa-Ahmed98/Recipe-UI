392c0dd5399983d8ae456eb767efe631e9409426
mohammed0xff <mohammed0xff@gmail.com>
2023-08-30 18:09:36 +0300

    add recipe listing functionality to index component

--- src/app/recipe-management/index/index.component.ts
+++ src/app/recipe-management/index/index.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+import { RecipeService } from '../../services/recipe.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  
+  recipes: Recipe[];
+  
+  constructor(
+    private recipeService: RecipeService, 
+    private router: Router,
+    )
+  {
+    this.recipes = recipeService.GetAllRecipes();
+  }
 
+  navigateToView(id: number){
+    this.router.navigate(['/recipes', id]);  
+  }
 }
--- src/app/recipe-management/recipe-management.module.ts
+++ src/app/recipe-management/recipe-management.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeRoutingModule } from './recipe-routing.module';
 
+// prime ng modules
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
 import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,9 @@ import { EditComponent } from './edit/edit.component';
   ],
   imports: [
     CommonModule,
-    RecipeRoutingModule
+    RecipeRoutingModule,
+    CardModule,
+    ButtonModule
   ]
 })
 export class RecipeManagementModule { }
