f8469688970fca9ee178e8a62043c43d814f1d89
mohammed0xff <mohammed0xff@gmail.com>
2023-09-02 20:09:16 +0300

    edited models to match with api models

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,10 +1,12 @@
+import { ingredient as Ingredient } from "./Ingredient.model";
 import { Category } from "./category.model";
+import { Step } from "./step.model";
 
 export interface Recipe{
     id: number,
     name: string,
-    image: string,
+    imageURL: string,
     category: Category,
-    ingredients: string[],
-    steps: string[],
+    ingredients: Ingredient[],
+    steps: Step[],
 }
